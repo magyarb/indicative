a71126a6fc608cbe454cbb820f6b6317f9cd4b33
balazs <magyarb94@gmail.com>
2017-06-02 05:02:36 +0200

    sql conn ok

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Handlebars     = require('handlebars');
+var HandlebarsIntl = require('handlebars-intl');
+var NumeralHelper = require("handlebars.numeral");
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+NumeralHelper.registerHelpers(Handlebars);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('handlebars', exphbs({defaultLayout:'layout',helpers: {
+  json: function (context) { return JSON.stringify(context); },
+  szazalek: function(context){return context/100 + "%";}
+}}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -41,6 +52,13 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+  console.log('Server started on port '+app.get('port'));
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,114 @@
 var express = require('express');
 var router = express.Router();
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+/*
+ var config = {
+ userName: 'testuser', // update me
+ password: 'test', // update me
+ server: '127.0.0.1', // update me
+ options: {
+ database: 'tdb' //update me
+ }
+ }
+ */
+
+const config = {
+  user: 'testuser',
+  password: 'test',
+  server: '127.0.0.1', // You can use 'localhost\\instance' to connect to named instance
+  database: 'tdb',
+  options: {
+    encrypt: false // Use this if you're on Windows Azure
+  }
+};
+
+/*
+ // Create connection to database
+ var config = {
+ userName: 'testuser', // update me
+ password: 'test', // update me
+ server: '127.0.0.1', // update me
+ options: {
+ database: 'tdb' //update me
+ }
+ }
+ var connection = new Connection(config);
+
+ // Attempt to connect and execute queries if connection goes through
+ connection.on('connect', function(err) {
+ if (err) {
+ console.log(err)
+ }
+ else{
+ queryDatabase()
+ }
+ });
+
+ function queryDatabase(){
+ console.log('Reading rows from the Table...');
+
+ // Read all rows from table
+ request = new Request(
+ "SELECT 1 as 'lofasz'",
+ function(err, rowCount, rows) {
+ console.log(rowCount + ' row(s) returned');
+ }
+ );
+
+ request.on('row', function(columns) {
+ columns.forEach(function(column) {
+ console.log("%s\t%s", column.metadata.colName, column.value);
+ });
+ });
+
+ connection.execSql(request);
+ };*/
+//creating connection pool
+const sql = require('mssql');
+var spool;
+sql.connect(config).then(pool => {
+  // Query
+  spool = pool;
+  return pool;
+});
+
+//Server=localhost;Database=master;Trusted_Connection=True;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  bool isRequest = false;
+  if (req.query.duration !== undefined)
+    isRequest =true;
+
+  spool.request()
+    .query("SELECT TOP (1000) [asd]  FROM [tdb].[dbo].[lofasz]").then(result => {
+    //handling results
+    var capex = [{item: "asd", price: "500"}, {item: "as4d", price: "5002"}];
+    var opex = [{item: "asd", price: "500"}, {item: "as4d", price: "5002"}];
+    var sumc = result.recordset[0].asd;
+    var sumo = result.recordset[1].asd;
+    res.render('index', {title: 'Express', capex: capex, opex: opex, sumc: sumc, sumo: sumo});
+  }).catch(err => {
+    // ... error checks
+    console.log(err);
+  });
+  /*
+   sql.connect(config).then(pool => {
+   // Query
+   return pool.request()
+   .query("SELECT TOP (1000) [asd]  FROM [tdb].[dbo].[lofasz]")
+   }).then(result => {
+   //handling results
+   var capex = [{item: "asd", price: "500"},{item: "as4d", price: "5002"}];
+   var opex = [{item: "asd", price: "500"},{item: "as4d", price: "5002"}];
+   var sumc = result.recordset[0].asd;
+   var sumo = result.recordset[1].asd;
+   res.render('index', { title: 'Express', capex: capex, opex: opex, sumc: sumc, sumo: sumo });
+   }).catch(err => {
+   // ... error checks
+   console.log(err);
+   });*/
 });
 
 module.exports = router;
